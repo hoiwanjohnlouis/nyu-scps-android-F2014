6b8982460bcb6d79c4d1d37970e2eeef318a0f67
hoiwanjohnlouis <hoiwanjohnlouis@gmail.com>
2014-10-18 16:55:57 -0400

    initial commit of S07E02_MyFragmentWalkthrough under the nyu-scps-android-F2014 repo.  it was incorrectly placed in the myDeitel repo.  this resolves the situation.

---
+++ S07E02_MyFragmentWalkthrough/app/src/androidTest/java/com/example/hoiwanlouis/s07e02_myfragmentwalkthrough/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.hoiwanlouis.s07e02_myfragmentwalkthrough;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
