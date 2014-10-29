5e0ec41b24a87c48dcd4448887c3d599e9868f2d
hoiwanjohnlouis <hoiwanjohnlouis@gmail.com>
2014-10-29 03:54:06 -0400

    completed code migration  for S08E02_MikeExternalStorage

--- S08E02_MikeExternalStorage/app/src/main/java/com/example/hoiwanlouis/s08e02_mikeexternalstorage/MyActivity.java
+++ S08E02_MikeExternalStorage/app/src/main/java/com/example/hoiwanlouis/s08e02_mikeexternalstorage/MyActivity.java
@@ -1,19 +1,69 @@
 package com.example.hoiwanlouis.s08e02_mikeexternalstorage;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 
 public class MyActivity extends Activity {
+
+    /* Checks if external storage is available for read and write */
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+
+    /* Checks if external storage is available to at least read */
+    public boolean isExternalStorageReadable() {
+        String state = Environment.getExternalStorageState();
+        if (
+                (Environment.MEDIA_MOUNTED.equals(state)) ||
+                (Environment.MEDIA_MOUNTED_READ_ONLY.equals(state))
+           ) {
+            return true;
+        }
+        return false;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
-    }
 
+        File dir = null;
+        dir = new File(
+                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_NOTIFICATIONS),
+                "my_own_directory"
+        );
+        if (!isExternalStorageWritable() || !dir.mkdirs()) {
+            Log.e("tag", "Directory not created");
+        }
+        File file = new File(dir, "myExternalData.txt");
+        String string = "I'm trapped in a file!";
+
+        try {
+            FileOutputStream f = new FileOutputStream(file);
+            f.write(string.getBytes());
+            f.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
