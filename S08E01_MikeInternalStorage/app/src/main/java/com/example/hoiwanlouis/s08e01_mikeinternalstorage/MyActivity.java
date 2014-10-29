3f2b513761085694ab538d18b32efd3d72507519
hoiwanjohnlouis <hoiwanjohnlouis@gmail.com>
2014-10-29 04:04:54 -0400

    completed code for S08E01_MikeInternalStorage

--- S08E01_MikeInternalStorage/app/src/main/java/com/example/hoiwanlouis/s08e01_mikeinternalstorage/MyActivity.java
+++ S08E01_MikeInternalStorage/app/src/main/java/com/example/hoiwanlouis/s08e01_mikeinternalstorage/MyActivity.java
@@ -1,9 +1,18 @@
 package com.example.hoiwanlouis.s08e01_mikeinternalstorage;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
 
 
 public class MyActivity extends Activity {
@@ -12,6 +21,48 @@ public class MyActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+
+        String filename = "myfile";
+        String string = "I'm trapped in a file!";
+        FileOutputStream outputStream;
+
+        Context context = this;
+        //File file = new File(context.getFilesDir(), filename);
+        File file = null;
+        try {
+            file = File.createTempFile(filename, null, context.getCacheDir());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            FileOutputStream f = new FileOutputStream(file);
+            f.write(string.getBytes());
+            f.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        StringBuilder text = new StringBuilder();
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                text.append(line);
+                text.append('\n');
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        TextView tv = (TextView)findViewById(R.id.text_view);
+        tv.setText(text);
+
+        file.delete();
     }
 
 
