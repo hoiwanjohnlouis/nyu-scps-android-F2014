f4621861286cab491ae03cb60b0f7bd10de3cf4e
hoiwanjohnlouis <hoiwanjohnlouis@gmail.com>
2014-10-30 06:25:37 -0400

    S08E03_MySQLite: ported DB code from Mike

---
+++ S08E23_MySQLite/app/src/main/java/com/example/hoiwanlouis/s08e23_mysqlite/FeedReaderContract.java
@@ -0,0 +1,19 @@
+package com.example.hoiwanlouis.s08e23_mysqlite; /**
+ * Created by Michael on 10/25/2014.
+ */
+
+import android.provider.BaseColumns;
+
+public final class FeedReaderContract {
+    // To prevent someone from accidentally instantiating the contract class,
+    // give it an empty constructor.
+    public FeedReaderContract() {}
+
+    /* Inner class that defines the table contents */
+    public static abstract class FeedEntry implements BaseColumns {
+        public static final String TABLE_NAME = "entry";
+        public static final String COLUMN_NAME_ENTRY_ID = "entryid";
+        public static final String COLUMN_NAME_TITLE = "title";
+        public static final String COLUMN_NAME_SUBTITLE = "subtitle";
+    }
+}
