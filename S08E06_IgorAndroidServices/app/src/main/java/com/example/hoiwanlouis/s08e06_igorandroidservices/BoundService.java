490759ce5a0cf56e2ade70be7be04ca35031383a
hoiwanjohnlouis <hoiwanjohnlouis@gmail.com>
2014-10-31 23:48:51 -0400

    S08E06_IgorAndroidServices: cloned igor's code.  added lines to gradle build.                          compile files('libs/support-v4.jar')                          compile files('libs/appcompat-v7.jar')                          compile 'com.squareup.okhttp:okhttp:2.0.0'                          compile 'com.google.code.gson:gson:2.3'

---
+++ S08E06_IgorAndroidServices/app/src/main/java/com/example/hoiwanlouis/s08e06_igorandroidservices/BoundService.java
@@ -0,0 +1,25 @@
+package com.example.hoiwanlouis.s08e06_igorandroidservices;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+
+public class BoundService extends Service {
+
+    private IBinder localBinder = new LocalBinder();
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return localBinder;
+    }
+
+    public class LocalBinder extends Binder {
+
+        public BoundService getBoundServerInstance() {
+            return BoundService.this;
+        }
+    }
+
+    //And then you'd have the rest of your Service code here...
+}
