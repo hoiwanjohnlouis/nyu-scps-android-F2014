97095faf5bfaf2f4cbdf4f6793b468bf5f6b485d
hoiwanjohnlouis <hoiwanjohnlouis@gmail.com>
2014-11-09 02:12:35 -0500

    S10E03_RobUserPrefsHeaders: migration complete, debugging (2)

--- S10E03_RobUserPrefsHeaders/app/src/main/java/com/example/hoiwanlouis/s10e03_robuserprefsheaders/UserPrefsActivity.java
+++ S10E03_RobUserPrefsHeaders/app/src/main/java/com/example/hoiwanlouis/s10e03_robuserprefsheaders/UserPrefsActivity.java
@@ -8,7 +8,7 @@ import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 
-public class UserPrefsHeadersActivity extends PreferenceActivity {
+public class UserPrefsActivity extends PreferenceActivity {
     /** Called when the activity is first created. */
 	@Override
     public void onCreate(Bundle savedInstanceState) {
